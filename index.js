6ef1ccde097d19aed611269bfd716f3b56a5529c
poopkinel <poopkinel@gmail.com>
2024-02-29 19:36:40 +0200

    remove app.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app..listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
