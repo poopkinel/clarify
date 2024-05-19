e00f7f03077778fdc2d83b7f644d59bf6558c2bf
poopkinel <poopkinel@gmail.com>
2024-05-19 22:45:11 +0300

    viewChatHistoryAsUserTest refactor

--- __tests__/useCases/User/viewChatHistoryAsUserTest.test.ts
+++ __tests__/useCases/User/viewChatHistoryAsUserTest.test.ts
@@ -5,27 +5,28 @@ import ViewChatHistoryAsUserRequestModel from "../../../src/dataModels/current/s
 import { ResponseType } from "../../../src/entities/responseEntity";
 
 class ViewChatHistoryAsUserTest {
+    private usecaseOutBoundary: any;
+
     setup() {
+        this.usecaseOutBoundary = {
+            sendResultModel: jest.fn()
+        }
     }
     
     setupAllDummies() {
-        const placeholderUserId: any = "userId";
-        const placeholderChatId: any = "chatId";
+        const dummyUserId: any = "userId";
+        const dummyChatId: any = "chatId";
         const dummyChatHistory: any = [];
-        const chatGateway: ChatGatewayToViewChatHistory = {
+        const dummychatGateway: ChatGatewayToViewChatHistory = {
             getChatHistoryById: jest.fn().mockResolvedValue(dummyChatHistory)
         };
         
-        const usecaseOutBoundary: any = {
-            sendResultModel: jest.fn()
-        };
-        
-        const viewChatHistoryAsUserUseCase = new ViewChatHistoryAsUserUseCase(usecaseOutBoundary, chatGateway);
-        const dummyRequestModel = new ViewChatHistoryAsUserRequestModel(placeholderChatId, placeholderUserId);
-        return { viewChatHistoryAsUserUseCase, dummyRequestModel, usecaseOutBoundary, placeholderUserId, dummyChatHistory };
+        const useCase = new ViewChatHistoryAsUserUseCase(this.usecaseOutBoundary, dummychatGateway);
+        const dummyRequestModel = new ViewChatHistoryAsUserRequestModel(dummyChatId, dummyUserId);
+        return { useCase, dummyRequestModel, placeholderUserId: dummyUserId, dummyChatHistory };
     }
     
-    setupUsecaseWithNonEmptyChatHistory(usecaseOutBoundary: any) {
+    setupUsecaseWithNonEmptyChatHistory() {
         const stubChatHistory: any = [
             {
                 id: "chatId",
@@ -42,7 +43,7 @@ class ViewChatHistoryAsUserTest {
             getChatHistoryById: jest.fn().mockResolvedValue(stubChatHistory)
         };
     
-        const useCase = new ViewChatHistoryAsUserUseCase(usecaseOutBoundary, chatGatewayStubHistory);
+        const useCase = new ViewChatHistoryAsUserUseCase(this.usecaseOutBoundary, chatGatewayStubHistory);
         return useCase;
     }
 
@@ -50,26 +51,25 @@ class ViewChatHistoryAsUserTest {
 
     runTests() {
         describe('Given a dummy chatGateway, usecaseOutBoundary, chatHistory and requestModel', () => {
-            var { viewChatHistoryAsUserUseCase, 
+            var { useCase: viewChatHistoryAsUserUseCase, 
                     dummyRequestModel, 
-                    usecaseOutBoundary, 
                     placeholderUserId, 
                     dummyChatHistory }: { 
-                        viewChatHistoryAsUserUseCase: ViewChatHistoryAsUserUseCase; 
+                        useCase: ViewChatHistoryAsUserUseCase; 
+                        placeholderUserId: any;
                         dummyRequestModel: ViewChatHistoryAsUserRequestModel; 
-                        usecaseOutBoundary: any; placeholderUserId: any; 
                         dummyChatHistory: any; } = this.setupAllDummies();
 
             it('should call usecaseOutBoundary.sendResultModel with a valid dummy response model', async () => {
                 await viewChatHistoryAsUserUseCase.executeViewChatHistory(dummyRequestModel);
         
-                expect(usecaseOutBoundary.sendResultModel).toHaveBeenCalledWith(
+                expect(this.usecaseOutBoundary.sendResultModel).toHaveBeenCalledWith(
                     new ViewChatHistoryAsUserResultModel(placeholderUserId, dummyChatHistory)
                 );
             });
 
             describe('Given only non-empty chatHistory', () => {
-                const useCase = this.setupUsecaseWithNonEmptyChatHistory(usecaseOutBoundary);
+                const useCase = this.setupUsecaseWithNonEmptyChatHistory();
 
                 it('should call usecaseOutBoundary.sendResultModel with a response model containing chat history', async () => {
                     await useCase.executeViewChatHistory(dummyRequestModel);
@@ -85,7 +85,7 @@ class ViewChatHistoryAsUserTest {
                         }]
                     }];
 
-                    expect(usecaseOutBoundary.sendResultModel).toHaveBeenCalledWith(
+                    expect(this.usecaseOutBoundary.sendResultModel).toHaveBeenCalledWith(
                         new ViewChatHistoryAsUserResultModel(
                             placeholderUserId,
                             expectedChatHistory
